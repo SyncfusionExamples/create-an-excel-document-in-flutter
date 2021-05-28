6b93aca8d1932354b64d95428514bfc3bdda05a1
Arulraj A <arul.raj47@gmail.com>
2021-05-28 15:52:30 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp"
+  "FLUTTER_TARGET=C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ArulrajA\\Desktop\\Flutter\\XlsIOGettingStarted\\Samples\\scripting-sample\\myflutterapp\\.dart_tool\\package_config.json"
+)
